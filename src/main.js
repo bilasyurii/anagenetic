7abc96839564e1ac25178fdd7df0d78f144354cb
bilasyurii <bilas.yurii@gmail.com>
2021-05-06 22:18:57 +0300

    Removed unused assets & preload state.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Engine from './anvas/engine.js';
-import PreloadState from './states/preload.js';
 import GameState from './states/game.js';
 import HashGrid from './anvas/physics/space-partitioning/hash-grid.js';
 // import Stats from './utils/stats.js';
@@ -8,9 +7,8 @@ new Engine()
   .setCanvasId('canvas')
   .setBodyColor('#222222')
   .setCanvasColor('#000000')
-  .registerState('preload', PreloadState)
   .registerState('game', GameState)
-  .setStartingState('preload')
+  .setStartingState('game')
   .setSpacePartitioning(new HashGrid())
   .start();
 

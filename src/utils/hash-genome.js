275c57dbb550b82dd1dd0a2efd90c4a8133096e2
bilasyurii <bilas.yurii@gmail.com>
2021-05-16 13:53:09 +0300

    Removed odd zero in hash.

--- src/utils/hash-genome.js
+++ src/utils/hash-genome.js
@@ -4,7 +4,7 @@ export default function hashGenome(genome) {
   const str = genome.toString();
   const hash32 = MurmurHash2(str, 1256);
   const base36 = hash32.toString(36).toUpperCase();
-  const padded = ('00000000' + base36).slice(-8);
+  const padded = ('000000' + base36).slice(-7);
 
   return '#' + padded;
 }
